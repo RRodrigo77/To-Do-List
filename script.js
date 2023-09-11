7acb707cc4a34c757a462834113fbfd8ecadc146
Rodrigo Rodrigues <rodrigo.rodrigues@activesoft.com.br>
2023-09-10 22:45:40 -0300

    Updade script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function getValueOfInput() {
-    let valorInput = document.getElementById('task').value;
-    console.log(valorInput);
+document.addEventListener("DOMContentLoaded", function() {
+    const taskInput = document.getElementById("taskInput"); // O campo de entrada
+    const taskForm = document.getElementById("taskForm"); // O formulário
 
-    if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {       
-        const task = valorInput;
-    }
-    
-    const listadetask = document.getElementById('listtask')
-    listadetask.innerHTML += `<li>${task}</li>`
-}
+    // Adicione um ouvinte de evento de envio ao formulário
+    taskForm.addEventListener("submit", function(event) {
+        event.preventDefault(); // Impedir o envio padrão do formulário
 
+        const valorInput = taskInput.value;
+
+        if (valorInput !== "") {       
+            const task = valorInput;
+            const listadetask = document.getElementById('listtask');
+            const listItem = document.createElement('li');
+            listItem.textContent = task;
+            listadetask.appendChild(listItem);
+
+            // Limpar o campo de entrada após adicionar a tarefa
+            taskInput.value = "";
+        }
+    });
+});
